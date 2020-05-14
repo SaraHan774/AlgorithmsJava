c8ec65f33a5cc966797edf815eca47409d4cb127
gahee <sarahan774@gmail.com>
2020-05-14 16:35:39 +0900

    update til

--- TIL/step11-recursion.md
+++ TIL/step11-recursion.md
@@ -16,5 +16,26 @@
     * 재귀로 풀이
 
 3. [별찍기](../src/com/gahee/algorithms/baek/A2447.java)
-    * 분할정복으로 푸는 것인데 ... 정답을 봐도 잘 모르겠다. 
+    * 분할정복으로 푸는 것인데 ... 정답을 봐도 잘 모르겠다.
+    
+* 2차원 배열의 length 
+    * `arr.length` 하면 행의 길이가 나옴. 
+    * `arr[row].length` 하면 열의 길이가 나옴. 
+    
+* `Arrays.fill()`
+
+```
+arr = new char[n][n];
+
+for (int i = 0; i < arr.length; i++) {
+    //각 행을 돌면서 i 번째 행의 모든 열을 빈 문자로 채운다. 
+    Arrays.fill(arr[i], ' '); 
+}
+```
+
+![star2477](../images/star-2477.png)
+    
+#### 2020-05-14 
+4. [하노이 탑]() 
+ 
 
--- src/com/gahee/algorithms/baek/A2447.java
+++ src/com/gahee/algorithms/baek/A2447.java
@@ -20,6 +20,9 @@ public class A2447 {
                 if(i == 1 && j == 1){
                     continue;
                 }
+//                System.out.printf("star(%d, %d, %d)\n", a + (div*i), b+(div*j), div);
+                // div * i 와 같이 출력하므로 i == j == 1 일때 즉, 3,3,3 와 같은 경우엔
+                // 별이 출력되지 않는다.
                 star(a + (div*i), b + (div*j), div);
             }
         }
@@ -32,6 +35,7 @@ public class A2447 {
         arr = new char[n][n];
 
         for (int i = 0; i < arr.length; i++) {
+            //각 행을 돌면서 i 번째 행의 모든 열을 빈 문자로 채운다.
             Arrays.fill(arr[i], ' ');
         }
 
