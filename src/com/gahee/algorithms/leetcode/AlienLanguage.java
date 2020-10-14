37e18a1d8f35be425f798d878c4c969a400c0dd0
sara <sarahan774@gmail.com>
2020-10-14 10:09:37 +0900

    Solve AlienLanguage

--- src/com/gahee/algorithms/leetcode/AlienLanguage.java
+++ src/com/gahee/algorithms/leetcode/AlienLanguage.java
@@ -14,8 +14,8 @@ public class AlienLanguage {
 
         //["fxasxpc","dfbdrifhp","nwzgs","cmwqriv","ebulyfyve","miracx","sxckdwzv","dtijzluhts","wwbmnge","qmjwymmyox"]
         //"zkgwaverfimqxbnctdplsjyohu"
-        String [] words = {"fxasxpc","dfbdrifhp","nwzgs","cmwqriv","ebulyfyve","miracx","sxckdwzv","dtijzluhts","wwbmnge","qmjwymmyox"};
-        String order = "zkgwaverfimqxbnctdplsjyohu";
+        String [] words = {"hello","leetcode"};
+        String order = "hlabcdefgijkmnopqrstuvwxyz";
 
         //h > l > a > b > ....
         //index
@@ -27,8 +27,8 @@ public class AlienLanguage {
         if(words.length == 1){
             return true;
         }
-
         char [] orderArr = order.toCharArray();
+
         HashMap<Character, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < orderArr.length; i++) {
             hashMap.put(orderArr[i], i);
@@ -37,23 +37,27 @@ public class AlienLanguage {
         for (int i = 0; i < words.length - 1; i++) {
             char [] first = words[i].toCharArray();
             char [] second = words[i + 1].toCharArray();
-            int len = Math.min(first.length, second.length);
 
-            for (int j = 0; j < len; j++) {
-                if(hashMap.get(first[j]) < hashMap.get(second[j])){
-                    return true;
-                }
-                else if(hashMap.get(first[j]) == hashMap.get(second[j])){
-                    continue;
-                }else{
-                    return false;
-                }
-            }
-
-            if(first.length <= second.length){
-                return true;
+            boolean isRightOrder = compare(first, second, hashMap);
+            if(!isRightOrder){
+                return false;
             }
         }
-        return false;
+        return true;
+    }
+
+    public static boolean compare(char [] first, char [] second, HashMap<Character, Integer> hashMap){
+        int len = Math.min(first.length, second.length);
+
+        for (int j = 0; j < len; j++) {
+            //사전순이라는게 맨 앞의 글자부터 시작해서 빨리 판별되도록
+            if(hashMap.get(first[j]) < hashMap.get(second[j])){
+                return true;
+            }
+            else if(hashMap.get(first[j]) > hashMap.get(second[j])){
+                return false;
+            }
+        }
+        return first.length <= second.length;
     }
 }
