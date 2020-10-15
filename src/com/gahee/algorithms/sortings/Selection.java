119e6b3fc99aa5e4d44726de07ffb9829b6e1d90
SaraHan774 <leisure306@gmail.com>
2020-10-15 14:55:17 +0900

    solve baek 2953, 15596

--- src/com/gahee/algorithms/sortings/Selection.java
+++ src/com/gahee/algorithms/sortings/Selection.java
@@ -2,19 +2,17 @@ package com.gahee.algorithms.sortings;
 
 public class Selection {
 
-    public static void main(String[] args) {
-
-    }
-
     public static void sort(Comparable [] a){
 
+        int idx = 0;
+        for (int i = idx + 1; i < a.length; i++) {
+
+        }
     }
 
-    private static boolean less(Comparable v, Comparable w){
-        return v.compareTo(w) < 0;
-    }
-
-    private static void exchange(Comparable [] a, int i, int j){
-
+    private static void exchange(Comparable[] a, int idx, int i) {
+        Comparable sub = a[idx];
+        a[idx] = a[i];
+        a[i] = sub;
     }
 }
