96155c739780f94a3d7dc0580d2b786c533b0b4c
Sara Han <sarahan774@gmail.com>
2023-09-25 23:40:29 +0900

    update

---
+++ src/com/gahee/algorithms/programmers/Shooting.java
@@ -0,0 +1,18 @@
+package com.gahee.algorithms.programmers;
+
+
+public class Shooting {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class Solution {
+
+        public int solution(int[][] targets) {
+            int answer = 0;
+            // target 들이 들어온다
+            return answer;
+        }
+    }
+}
